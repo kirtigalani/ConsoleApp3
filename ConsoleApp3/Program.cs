12249ebc6d762cb9d1ab97433fdfd3560aaeab2e
kirtigalani <kittugalani@gmail.com>
2024-11-28 22:40:30 +0530

    remove line

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -7,7 +7,6 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
         }
     }
 }
